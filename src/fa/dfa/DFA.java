41083a80ee9d616cbc59c001c89879ff0ce7c757
Mudvayne <Mudvayneofmud@users.noreply.github.com>
2021-09-20 22:12:57 -0600

    Required Classes

--- src/fa/dfa/DFA.java
+++ src/fa/dfa/DFA.java
@@ -2,4 +2,29 @@ package fa.dfa;
 
 public class DFA {
 
+	public void addFinalState(String nextToken) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addStartState(String startStateName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addState(String nextToken) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addTransition(String valueOf, char c, String valueOf2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean accepts(String nextLine) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
 }
