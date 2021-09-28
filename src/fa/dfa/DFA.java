c21a31a2612fbae481acbb037c239203ab4bb8be
Mudvayne <Mudvayneofmud@users.noreply.github.com>
2021-09-27 22:48:09 -0600

    Update DFA.java
    Corrections, semicolons, errors all corrected.

--- src/fa/dfa/DFA.java
+++ src/fa/dfa/DFA.java
@@ -1,23 +1,31 @@
 package fa.dfa;
 
-public class DFA {
+import java.util.Set;
+
+import fa.State;
+
+public class DFA implements DFAInterface{
 	Set <DFAState> finalStates;
+	Set <DFAState> startStates;
+	Set <DFAState> states;
 	DFAState start;
 
 	public void addFinalState(String nextToken) {
 		// TODO Auto-generated method stub
-		DFAState final = new DFAState (nextToken, true); 
-		finalStates.add(final);
+		DFAState _final = new DFAState (nextToken, true); 
+		finalStates.add(_final);
 		
 	}
 
 	public void addStartState(String startStateName) {
 		// TODO Auto-generated method stub
-		
+		DFAState start = new DFAState (startStateName, true); 
+		finalStates.add(start);
 	}
 
 	public void addState(String nextToken) {
-		// TODO Auto-generated method stub
+		DFAState state = new DFAState (nextToken, true); 
+		finalStates.add(state);
 		
 	}
 
@@ -30,6 +38,36 @@ public class DFA {
 	public boolean accepts(String nextLine) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public Set<? extends State> getStates() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<? extends State> getFinalStates() {
+		// TODO Auto-generated method stub
+		return finalStates;
+	}
+
+	@Override
+	public State getStartState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Character> getABC() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public State getToState(DFAState from, char onSymb) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
