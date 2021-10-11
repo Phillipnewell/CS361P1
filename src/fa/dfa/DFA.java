13559bbd9d8465911e9166d2f0186917374a8b32
Mudvayne <Mudvayneofmud@users.noreply.github.com>
2021-10-11 10:58:16 -0600

    Update DFA.java

--- src/fa/dfa/DFA.java
+++ src/fa/dfa/DFA.java
@@ -1,73 +1,96 @@
 package fa.dfa;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 import fa.State;
 
 public class DFA implements DFAInterface{
-	Set <DFAState> finalStates;
-	Set <DFAState> startStates;
-	Set <DFAState> states;
-	DFAState start;
+	Set<Character> sigma = new HashSet<Character>();//My abc's of the language
+	Set<DFAState> allStates = new HashSet<DFAState>();
+	Set<DFAState> finalStates = new HashSet<DFAState>();
+	HashMap<String, DFAState> lookup = new HashMap<String, DFAState>();
+	DFAState startState;
 
 	public void addFinalState(String nextToken) {
-		// TODO Auto-generated method stub
-		DFAState _final = new DFAState (nextToken, true); 
-		finalStates.add(_final);
-		
+		DFAState finalState = new DFAState(nextToken);
+		finalState.setIsFinal(true);//flag final as true
+		allStates.add(finalState);//Q
+		finalStates.add(finalState);//F
+		lookup.put(nextToken, finalState);
 	}
 
 	public void addStartState(String startStateName) {
-		// TODO Auto-generated method stub
-		DFAState start = new DFAState (startStateName, true); 
-		finalStates.add(start);
+		if (lookup.get(startStateName) != null){
+			//start state is already in there
+			DFAState startState = lookup.get(startStateName);
+			this.startState = startState;
+		} else {
+			//start state is new and isFinal=false
+			DFAState startState = new DFAState(startStateName);
+			startState.setIsFinal(false);
+			lookup.put(startStateName, startState);
+			this.startState = startState;
+			allStates.add(startState);
+		}	
+	}
+
+	public void addTransition(String valueOf1, char c, String valueOf2) {
+		sigma.add(c);//list of abc's of language
+		DFAState fromState = lookup.get(valueOf1);
+		DFAState toState = lookup.get(valueOf1);
+		fromState.addTransition(c,toState);
 	}
 
 	public void addState(String nextToken) {
-		DFAState state = new DFAState (nextToken, true); 
-		finalStates.add(state);
-		
-	}
-
-	public void addTransition(String valueOf, char c, String valueOf2) {
-		// TODO Auto-generated method stub
-		
-		
-	}
+		DFAState transitionState = new DFAState(nextToken);
+		transitionState.setIsFinal(false);
+		allStates.add(transitionState);
+		lookup.put(nextToken, transitionState);
+		}
 
 	public boolean accepts(String nextLine) {
-		// TODO Auto-generated method stub
-		return false;
+		System.out.println("here" + nextLine);
+		DFAState nextState = lookup.get(nextLine);
+		return nextState.getIsFinal();
 	}
 
 	@Override
 	public Set<? extends State> getStates() {
-		// TODO Auto-generated method stub
-		return null;
+		return allStates;
 	}
 
 	@Override
 	public Set<? extends State> getFinalStates() {
-		// TODO Auto-generated method stub
 		return finalStates;
 	}
 
 	@Override
 	public State getStartState() {
-		// TODO Auto-generated method stub
-		return null;
+		return startState;
 	}
 
 	@Override
 	public Set<Character> getABC() {
-		// TODO Auto-generated method stub
-		return null;
+		return sigma;
 	}
 
 	@Override
 	public State getToState(DFAState from, char onSymb) {
-		// TODO Auto-generated method stub
-		return null;
+		DFAState toState = from.transitionTo.get(onSymb);
+		return toState;
+	}
+	
+	public String toString() {
+		System.out.println("Q= {" + allStates + "}");
+		System.out.println("Sigma = {" + getABC() + "}");
+		System.out.println("Delta");
+		
+		//rubbish code
+		String cheese = "cheese";
+		return cheese;
+		
 	}
 
 }
